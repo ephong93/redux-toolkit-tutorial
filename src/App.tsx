f6625946ca7d53b5189d64974eda1c2c34775f50
ephong93 <ephong93@gmail.com>
2022-04-23 16:23:34 +0900

    Add pokemon API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useGetPokemonByNameQuery } from './services/pokemon';
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
   return (
     <div className="App">
       <Counter />
+      
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import counterReducer from '../features/counter/counterSlice';
+import { pokemonApi } from '../services/pokemon';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
-  }
+    counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware)
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AddDispatch = typeof store.dispatch;
